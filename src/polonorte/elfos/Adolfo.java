b325b2a52d8df2f75c38ee6a757cbc52ccdece29
Adolfo0024 <correobasura960@gmail.com>
2024-02-20 13:53:03 +0100

    Segunda prueba

---
+++ src/polonorte/elfos/Adolfo.java
@@ -0,0 +1,8 @@
+package polonorte.elfos;
+
+public class Adolfo extends ElfoGenerico {
+
+	public Adolfo() {
+		super("Adolfo", SeccionLaboralElfo.DISFRACES);
+	}
+}
--- src/polonorte/elfos/DiegoA.java
+++
@@ -1,8 +0,0 @@
-package polonorte.elfos;
-
-public class DiegoA extends ElfoGenerico {
-
-	public DiegoA() {
-		super("Diegu", SeccionLaboralElfo.DISFRACES);
-	}
-}
